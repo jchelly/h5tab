efde677ad2946af188349b43d7d97d3e8e0aba8f
John Helly <j.c.helly@durham.ac.uk>
2023-03-03 13:00:03 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='h5tab',
+      version='0.1',
+      description='View HDF5 datasets as table columns',
+      author='John Helly',
+      author_email='j.c.helly@durham.ac.uk',
+      scripts=["h5tab",],
+     )
